dfc08b9422e85cb5ffc0059371d26b595d8fd94f
closeup1202 <closeup1202@gmail.com>
2024-07-03 22:52:12 +0900

    test2

--- src/operator/scramble.ts
+++ src/operator/scramble.ts
@@ -6,32 +6,34 @@ export default class Scrambler {
     return new Scrambler();
   }
 
-  static CHARACTORS: string[] = [
+  static CHARACTORS = [
     '@', '#', '$', '%', '£', '&', '*', '§', '+', '_','^','!','~',
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
     'ㄱ','ㄲ','ㄴ','ㄷ','ㄸ','ㄹ','ㅁ','ㅂ','ㅃ','ㅅ','ㅆ','ㅇ','ㅈ',
     'ㅉ','ㅊ','ㅋ','ㅌ','ㅍ','ㅎ','ㅏ','ㅐ','ㅑ','ㅒ','ㅓ','ㅔ','ㅕ',
     'ㅖ','ㅗ','ㅘ','ㅙ','ㅚ','ㅛ','ㅜ','ㅝ','ㅞ','ㅟ','ㅠ','ㅡ','ㅢ','ㅣ',
-  ];
+  ] as Char[]
 
-  sourceChars: string | undefined;
-  targetChar: Char | undefined
+  targetChar: Char;
+  targetIdx: number;
   counter: number;
-  onScramble: React.Dispatch<React.SetStateAction<string>> | null;
-  frameId: number | null;
-  frameIndex: number;
+  randomChars: Char[]
+  onScramble: React.Dispatch<React.SetStateAction<Char[]>> | null;
+  frameId: number;
 
   constructor(){
-    this.counter = 12;
-    this.sourceChars = undefined;
+    this.targetChar = '' as Char
+    this.targetIdx = 0;
+    this.counter = 20;
+    this.randomChars = [];
     this.onScramble = null;
-    this.frameId = null;
-    this.frameIndex = 0;
+    this.frameId = 0;
   }
 
-  target(char: Char){
-    this.targetChar = char
+  target(char: Char, idx:number){
+    this.targetChar = char;
+    this.targetIdx = idx;
     return this;
   }
 
@@ -40,26 +42,26 @@ export default class Scrambler {
     return this;
   }
 
-  private select(){
-    let temp = '';
-    if(this.counter){
-      while(this.counter-- > 0){
-        const idx = Math.floor(Math.random() * Scrambler.CHARACTORS.length - 1) + 1;
-        temp += Scrambler.CHARACTORS[idx]
-      }
+  generator(){
+    while(this.counter > 0){
+      const idx = Math.floor(Math.random() * Scrambler.CHARACTORS.length - 1) + 1;
+      this.randomChars.push(Scrambler.CHARACTORS[idx])
+      this.counter--;
     }
-    return temp + this.targetChar;
+    this.randomChars.push(this.targetChar)
   }
 
-  scramble(onScramble: React.Dispatch<React.SetStateAction<string>>){
-    this.sourceChars = this.select();
+  scramble(onScramble: React.Dispatch<React.SetStateAction<Char[]>>){
+    this.generator();
     this.onScramble = onScramble
     this.frameId = requestAnimationFrame(() => this.change());
   }
-
   change(){
-    if(this.onScramble && this.sourceChars){
-      this.onScramble(this.sourceChars[1])
+    if(this.onScramble && this.randomChars.length > 0){
+      this.onScramble((prev:any) => prev.map((item: Char, index: number) => index === this.targetIdx ? this.randomChars.shift() : item))
+      this.frameId = requestAnimationFrame(() => this.change());
+    } else {
+      cancelAnimationFrame(this.frameId)
     }
   }
 }
--- src/pages/Word.tsx
+++ src/pages/Word.tsx
@@ -5,30 +5,39 @@ import { Char } from '../types/char';
 
 export default function Word() {
 
-  const [value, setValue] = useState<string[]>([]);
-  const scramblerRef = useRef(Scrambler.instance())
+  const [value, setValue] = useState<Char[]>([]);
+  const lastExecuted = useRef<number>(0);
+  const interval = 500;
 
   const init = async () => {
     const word = await getWords();
-    setValue([...word]);
-    console.log(value)
+    setValue([...word] as Char[]);
   }
 
-  // const handleMouseOver = (char: Char) => {
-  //   scramblerRef.current
-  //   .target(char)
-  //   .scramble(setValue)
-  // }
+  const handleMouseOver = (char: Char, idx: number) => {
+    const now = Date.now();
+    if (now - lastExecuted.current > interval) {
+      lastExecuted.current = now;
+      Scrambler.instance()
+        .target(char, idx)
+        .scramble(setValue);
+    }
+  }
 
   useEffect(() => {
     init();
   }, [])
 
   return (
-    <div className="flex items-center justify-center h-screen text-[20px] tracking-[-0.5px] cursor-default"> 
-      <div>
-      { value || 'loading...'} 
-      </div>
+    <div className="flex items-center justify-center h-screen text-[30px] tracking-[-0.5px] cursor-default"> 
+      {
+        !!value && value.map((char, idx) => 
+          <span 
+            key={idx} 
+            onMouseOver={() => handleMouseOver(char, idx)}
+          >{char}</span>
+        )
+      }
     </div>
   )
 }
